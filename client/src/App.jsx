2ea88e16ef2fde787dc36cd0a0c2baaba2605f8c
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-10 09:54:19 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 font-sans p-6">
-      <Dashboard />
+    <div className="min-h-screen bg-gray-100 text-gray-900 font-sans">
+      <Navbar />
+      <div className="p-6">
+        <Dashboard />
+      </div>
     </div>
   );
 }
