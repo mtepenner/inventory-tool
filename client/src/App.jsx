93c98f24e8a46f46082df55b1bdaeead22cff1a3
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-10 09:49:53 -0700

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 font-sans p-6">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
